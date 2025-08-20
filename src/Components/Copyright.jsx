987194ad838ddd3e86a6212d2cc01487a111f204
SohaibRaza66 <sohaibraza236@gmail.com>
2025-08-20 12:48:44 +0500

    first commit

--- src/Components/Copyright.jsx
+++ src/Components/Copyright.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 const Copyright = () => {
   return (
     <>
-    <div className='bg-gray-100'>
-    <div className="copyright container m-auto w-full flex items-center justify-between p-4">
-        <p className='text-start w-max'>&copy; 2023 ECommerce.</p>
-        <p className='text-end flex w-max items-center'><img src="US@2x.jpg" alt="" className='w-4' /> English <MdKeyboardArrowDown /></p>
-    </div>
-    </div>
+      <div className="bg-gray-100">
+        <div className="copyright container m-auto w-full flex items-center justify-between p-4">
+          <p className="text-start w-max">&copy; 2023 ECommerce.</p>
+          <p className="text-end flex w-max items-center">
+            <img src="US@2x.jpg" alt="" className="w-4" /> English{" "}
+            <MdKeyboardArrowDown />
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Copyright
+export default Copyright;
