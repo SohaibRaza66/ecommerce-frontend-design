e3d6a5ee4c32c4ab73fcd0b146748d3b1caeac41
SohaibRaza66 <sohaibraza236@gmail.com>
2025-08-21 13:14:32 +0500

    first commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <div className="container bg-white md:flex grid m-auto p-5 mt-4 rounded-md">
+        <div className="banner-item md:w-2/12 w-full p-4">
+          <ul>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Automobiles
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Clothes and Wear
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Home interiors
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Computer and Tech
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Tools , Equipment
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Sports and Outdoor
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Animal and Pets
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              Machinery Tools
+            </li>
+            <li className="mt-4 hover:bg-blue-200 cursor-pointer">
+              More Category
+            </li>
+          </ul>
+        </div>
+        <div className="banner-item relative md:w-7/12 w-full p-4 grid justify-center items-center">
+          <img src="Banner.png" alt="Banner" className="relative h-full" />
+          <p className="text absolute top-10 start-20 md:text-2xl">
+            Latest Trending
+          </p>
+          <p className="text absolute top-25 start-20 md:text-3xl font-bold">
+            Electronic Items
+          </p>
+          <button className="absolute top-40 start-20 bg-gray-200 p-2 rounded-md cursor-pointer">
+            Learn More
+          </button>
+        </div>
+        <div className="banner-item md:w-3/12 w-full p-4">
+        <div className="user bg-blue-200 p-4 rounded-md">
+          <div className="usr flex">
+          <img src="Avatar.png" alt="User" className="h-auto" />
+          <div className="txt">
+          <p className="text">Hi , User</p>
+          <p className="text">Let's get Started</p>
+          </div>
+          </div>
+          <div className="btns grid">
+          <button className="bg-blue-500 p-2 rounded-md cursor-pointer mt-2 text-white">Join Now</button>
+          <button className="bg-gray-100 p-2 rounded-md cursor-pointer mt-2 text-blue-500">Login</button>
+          </div>
+        </div>
+        <div className="text rounded-md bg-orange-400 p-5 mt-5 text-white">
+          <p className="text w-1/3">Get US $10 off with a new supplier</p>
+        </div>
+        <div className="text rounded-md bg-teal-400 p-5 mt-5 text-white">
+          <p className="text w-1/3">Send quotes with supplier preferences</p>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/Components/Offers.jsx
+++ src/Components/Offers.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Offers = () => {
   return (
-    <div>Offers</div>
+    <>
+    <div className="container bg-white rounded-md mt-4 flex m-auto p-4">
+      <div className="time">
+        <h2 className="text-2xl font-semi-bold">Deals and Offer</h2>
+        <p className='text-gray-400'>Hygiene Equipments</p>
+        <div className="timer flex mt-2">
+          <p className='grid ms-2 justify-center items-center text-center p-2 rounded-md bg-gray-500 text-white'>00 <span>Days</span></p>
+          <p className='grid ms-2 justify-center items-center text-center p-2 rounded-md bg-gray-500 text-white'>00 <span>Hours</span></p>
+          <p className='grid ms-2 justify-center items-center text-center p-2 rounded-md bg-gray-500 text-white'>00 <span>Minutes</span></p>
+          <p className='grid ms-2 justify-center items-center text-center p-2 rounded-md bg-gray-500 text-white'>00 <span>Seconds</span></p>
+        </div>
+      </div>
+      <div className="img w-3/12 p-4">
+        <img src="image 35.jpg" alt="Offer 1" className='w-40 h-40 object-contain m-auto' />
+        <p className='m-auto w-max mt-2 mb-2'>Smart Watches</p>
+        <p className='bg-red-300 text-red-600 w-max px-4 py-2 rounded-4xl m-auto'>-25%</p>
+      </div>
+      <div className="img w-3/12 p-4">
+        <img src="image 34.jpg" alt="Offer 2" className='w-40 h-40 object-contain m-auto' />
+        <p className='m-auto w-max mt-2 mb-2'>Laptops</p>
+        <p className='bg-red-300 text-red-600 w-max px-4 py-2 rounded-4xl m-auto'>-15%</p>
+      </div>
+      <div className="img w-3/12 p-4">
+        <img src="image 28.jpg" alt="Offer 3" className='w-40 h-40 object-contain m-auto' />
+        <p className='m-auto w-max mt-2 mb-2'>GoPro Cameras</p>
+        <p className='bg-red-300 text-red-600 w-max px-4 py-2 rounded-4xl m-auto'>-40%</p>
+      </div>
+      <div className="img w-3/12 p-4">
+        <img src="image 29.jpg" alt="Offer 4" className='w-40 h-40 object-contain m-auto' />
+        <p className='m-auto w-max mt-2 mb-2'>HeadPhones</p>
+        <p className='bg-red-300 text-red-600 w-max px-4 py-2 rounded-4xl m-auto'>-25%</p>
+      </div>
+      <div className="img w-3/12 p-4">
+        <img src="image 23.jpg" alt="Offer 4" className='w-40 h-40 object-contain m-auto' />
+        <p className='m-auto w-max mt-2 mb-2'>Canon Cameras</p>
+        <p className='bg-red-300 text-red-600 w-max px-4 py-2 rounded-4xl m-auto'>-25%</p>
+      </div>
+      </div>
+    </>
   )
 }
 
